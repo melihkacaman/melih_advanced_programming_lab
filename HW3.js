7b25a8b677001c2b4ea8afc4616fc5d8e7a9962c
melih.kacaman <melih.kacaman@stu.fsm.edu.tr>
2022-11-20 17:49:12 +0300

    deleted sprite

--- HW3.js
+++ HW3.js
@@ -1,12 +1,28 @@
-const canvas = document.getElementById("canvas"); 
-const CANVAS_WITH = canvas.with = CANVAS_HEIGHT = canvas.height = 600; 
+const canvas = document.getElementById("canvas");
+const CANVAS_WITH = canvas.with = CANVAS_HEIGHT = canvas.height = 600;
+const BASE_URL = "https://melihkacaman.github.io/melih_advanced_programming_lab/sprites/";
+const playerIMG = document.getElementById("player");
+const sprites = [];
 
+function main() {
+    for (let i = 0; i < 10; i++) {
+        const imageUrl = `${BASE_URL}${i}.png`;
+        fetch(imageUrl)
+            .then(response => response.blob())
+            .then(blob => {
+                const sprite = new Image();
+                sprite.src = URL.createObjectURL(blob);
+                sprites.push(sprite);
+            });;
+    }
 
-function main(){
-    fetch("https://static.euronews.com/articles/stories/05/62/57/76/1440x810_cmsv2_8331d75a-ad74-5b67-9f63-7ec263748179-5625776.jpg").then(response => {
-        console.log(response); 
-    })
 }
 
-main(); 
+function fun(array) {
+    array.forEach(element => {
+        console.log(element);
+    });
+}
+
+main();
 
